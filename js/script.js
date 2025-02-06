6b422395e520d4943a2a45680d44ba129cceff1c
roberto.mottura <roberto.mottura@educ.di.unito.it>
2025-02-06 22:37:09 +0100

    prova

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     function updateImage() {
         if ($(window).width() <= 767) {
-            $('.personal-responsive-img').attr('src', 'img/222logo-white.png');
+            //$('.personal-responsive-img').attr('src', 'img/222logo-white.png');
             $(window).scroll(function() {
                 if ($(this).scrollTop() > 3) { // Cambia il valore 100 in base a quanto vuoi che l'utente scrolli
                     $('#main-content').addClass('visible-content');
@@ -13,14 +13,22 @@ $(document).ready(function() {
                 }
             });
         } else {
-            $('.personal-responsive-img').attr('src', 'img/222logo-white.png');
+            //$('.personal-responsive-img').attr('src', 'img/222logo-white.png');
             $('#main-content').addClass('visible-content');
             $('.hero-header').removeClass('dimmed');
         }
     }
-    
 
+    function updateBackgroundPosition() {
+        if ($(window).width() <= 767) {
+            var scrollTop = $(window).scrollTop();
+            $('.hero-header').css('background-position', '50% ' + (scrollTop * 0.5) + 'px');
+        }
+    }
+    
     updateImage(); // Chiamata iniziale
+    updateBackgroundPosition();
+    $(window).scroll(updateBackgroundPosition);
     $(window).resize(updateImage); // Chiamata al ridimensionamento
     console.log('script.js loaded');
 });
