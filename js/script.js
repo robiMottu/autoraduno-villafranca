ac4579d2141cdc1405671deb1d7351be7b4b1b37
roberto.mottura <roberto.mottura@educ.di.unito.it>
2025-02-07 12:09:17 +0100

    initial commit again

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     function updateImage() {
         if ($(window).width() <= 767) {
-            //$('.personal-responsive-img').attr('src', 'img/222logo-white.png');
+            $('.personal-responsive-img').attr('src', 'img/222logo-white.png');
             $(window).scroll(function() {
                 if ($(this).scrollTop() > 3) { // Cambia il valore 100 in base a quanto vuoi che l'utente scrolli
                     $('#main-content').addClass('visible-content');
@@ -13,14 +13,14 @@ $(document).ready(function() {
                 }
             });
         } else {
-            //$('.personal-responsive-img').attr('src', 'img/222logo-white.png');
+            $('.personal-responsive-img').attr('src', 'img/222logo-white.png');
             $('#main-content').addClass('visible-content');
             $('.hero-header').removeClass('dimmed');
         }
     }
     
+
     updateImage(); // Chiamata iniziale
-    //$(window).resize(updateImage); // Chiamata al ridimensionamento
-    $(window).on('resize', updateImage)
+    $(window).resize(updateImage); // Chiamata al ridimensionamento
     console.log('script.js loaded');
 });
