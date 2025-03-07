cfd7d175db8051448328d14249f7c76d191e1985
roberto.mottura <roberto.mottura@educ.di.unito.it>
2025-03-07 23:55:53 +0100

    mobile width problem fix and mobile scroll effect disable

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     function updateImage() {
         if ($(window).width() <= 767) {
-            $('.personal-responsive-img').attr('src', 'img/222logo-white.png');
+            /*$('.personal-responsive-img').attr('src', 'img/222logo-white.png');
             $(window).scroll(function() {
                 if ($(this).scrollTop() > 3) { // Cambia il valore 100 in base a quanto vuoi che l'utente scrolli
                     $('#main-content').addClass('visible-content');
@@ -11,7 +11,7 @@ $(document).ready(function() {
                     $('#main-content').removeClass('visible-content');
                     $('.fixed-bg').removeClass('dimmed');
                 }
-            });
+            });*/
         } else {
             $('.personal-responsive-img').attr('src', 'img/222logo-white.png');
             $('#main-content').addClass('visible-content');
